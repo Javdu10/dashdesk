35b9b7a41682774d5500de9c13f564f27a32c7f0
Javdu10 <jav.valentin@gmail.com>
2020-07-01 15:53:38 +0200

    update

--- tests/Browser/ExampleTest.php
+++ tests/Browser/ExampleTest.php
@@ -16,8 +16,7 @@ class ExampleTest extends DuskTestCase
     public function testBasicExample()
     {
         $this->browse(function (Browser $browser) {
-            $browser->visit('/')
-            ->dump();
+            $browser->visit('/');
             $this->assertTrue(true);
         });
     }
